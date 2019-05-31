d99053ff48904a3864ca39591a5d9d70c80e99a5
范有江 <fanyoujiang@bilibili.com>
2019-05-31 11:19:35 +0800

    Add admin filter

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -5,9 +5,26 @@ from .models import Post, Category, Tag
 
 # Register your models here.
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """define the filter to show the owner category"""
+    
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+    
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time')
+    list_filter = [CategoryOwnerFilter]
+
     fields = ('name', 'status', 'is_nav')
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
@@ -25,7 +42,7 @@ class TagAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'operator'
+        'created_time', 'owner', 'operator'
     ]
     list_display_links = []
     list_filter =['category', ]
@@ -50,6 +67,11 @@ class PostAdmin(admin.ModelAdmin):
             reverse('admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = '操作'
+
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+    
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
